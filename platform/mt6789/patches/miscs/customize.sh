f1801f9a40f901de036122bb79315c6c8356917b
leynox64 <chinoidlacaille0@gmail.com>
2025-06-07 20:23:42 +0200

    a15
    add a15 stuffs (except midas)

---
+++ platform/mt6789/patches/miscs/customize.sh
@@ -0,0 +1,9 @@
+echo "Disable Vulkan"
+SET_PROP "vendor" "ro.hwui.use_vulkan" "false"
+SET_PROP "vendor" "debug.hwui.renderer" "skiagl"
+SET_PROP "vendor" "debug.renderengine.backend" "skiagl"
+SET_PROP "vendor" "renderthread.skia.reduceopstasksplitting" "true"
+SET_PROP "vendor" "debug.hwui.skia_atrace_enabled" "false"
+
+echo "Disabling A/B"
+SET_PROP "product" "ro.product.ab_ota_partitions" --delete
