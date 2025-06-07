ae4ca66dfaf1faf58f5d100b931979b3f107a334
leynox64 <chinoidlacaille0@gmail.com>
2025-06-07 20:33:53 +0200

    add mtk support

--- unica/patches/_desixtification/customize.sh
+++ unica/patches/_desixtification/customize.sh
@@ -1,6 +1,32 @@
 if [[ $TARGET_SINGLE_SYSTEM_IMAGE == "qssi" || $TARGET_SINGLE_SYSTEM_IMAGE == "essi" || $TARGET_SINGLE_SYSTEM_IMAGE == "mssi" ]]; then
     echo "Target device with 32-Bit HALs detected! Patching..."
 
+if [ $TARGET_CODENAME == "a15" ]; then
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib" 0 0 644 "u:object_r:system_lib_file:s0"
+
+    BLOBS_LIST="
+    system/apex/com.android.i18n.apex
+    system/apex/com.android.runtime.apex
+    system/apex/com.google.android.tzdata6.apex
+    system/bin/linker
+    system/bin/linker_asan
+    system/bin/bootstrap/linker
+    system/bin/bootstrap/linker_asan
+    "
+    for blob in $BLOBS_LIST
+    do
+        ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "$blob" 0 0 644 "u:object_r:system_file:s0"
+    done
+
+    BLOBS_LIST="
+    system/bin/linker
+    system/bin/bootstrap/linker
+    "
+    for blob in $BLOBS_LIST
+    do
+        ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "$blob" 0 0 644 "u:object_r:system_linker_exec:s0"
+    done
+else
     ADD_TO_WORK_DIR "dm3qxxx" "system" "system/lib" 0 0 644 "u:object_r:system_lib_file:s0"
 
     BLOBS_LIST="
@@ -25,6 +51,7 @@ if [[ $TARGET_SINGLE_SYSTEM_IMAGE == "qssi" || $TARGET_SINGLE_SYSTEM_IMAGE == "e
     do
         ADD_TO_WORK_DIR "dm3qxxx" "system" "$blob" 0 0 644 "u:object_r:system_linker_exec:s0"
     done
+fi
 
     # Set props
     echo "Setting props..."
