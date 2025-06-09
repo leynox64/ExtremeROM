da656cedb2c0c01004f72dc827c980077b6a80ef
leynox64 <chinoidlacaille0@gmail.com>
2025-06-09 21:14:15 +0200

    hi?

---
+++ target/a14x/config.sh
@@ -0,0 +1,58 @@
+#
+# Copyright (C) 2024 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy A14 5G (a14x)
+TARGET_NAME="Galaxy A14 5G"
+TARGET_CODENAME="a14x"
+TARGET_PLATFORM="exynos1330"
+TARGET_FIRMWARE="SM-A146M/ZTO/350003441234564"
+TARGET_EXTRA_FIRMWARES=("")
+TARGET_API_LEVEL=34
+TARGET_PRODUCT_FIRST_API_LEVEL=33
+TARGET_VNDK_VERSION=33
+TARGET_SINGLE_SYSTEM_IMAGE="essi"
+TARGET_OS_FILE_SYSTEM="erofs"
+TARGET_SUPER_GROUP_NAME="group_basic"
+TARGET_SUPER_PARTITION_SIZE=8287944704
+TARGET_SUPER_GROUP_SIZE=8283750400
+TARGET_HAS_SYSTEM_EXT=false
+TARGET_INSTALL_METHOD=zip
+TARGET_BOOT_DEVICE_PATH="/dev/block/by-name"
+
+# SEC Product Feature
+TARGET_AUTO_BRIGHTNESS_TYPE="5"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_default"
+TARGET_NFC_CHIP_VENDOR="none"
+TARGET_FP_SENSOR_CONFIG="google_touch_display_side,settings=3"
+TARGET_HAS_MASS_CAMERA_APP=true
+TARGET_HAS_QHD_DISPLAY=false
+TARGET_HFR_MODE="2"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60,90"
+TARGET_HFR_DEFAULT_REFRESH_RATE="90"
+TARGET_DISPLAY_CUTOUT_TYPE="center"
+TARGET_IS_ESIM_SUPPORTED=false
+TARGET_HAS_HW_MDNIE=false
+TARGET_MDNIE_SUPPORTED_MODES="0"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="0"
+TARGET_SUPPORT_WIFI_7=false
+TARGET_SUPPORT_HOTSPOT_DUALAP=false
+TARGET_SUPPORT_HOTSPOT_WPA3=true
+TARGET_SUPPORT_HOTSPOT_6GHZ=false
+TARGET_SUPPORT_HOTSPOT_WIFI_6=true
+TARGET_SUPPORT_HOTSPOT_ENHANCED_OPEN=false
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
