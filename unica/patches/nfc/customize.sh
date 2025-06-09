d4e4bd79dbaaf4286d038c17765a8cc969539c5c
leynox64 <chinoidlacaille0@gmail.com>
2025-06-09 22:01:38 +0200

    remove more nfc libs if no nfc

--- unica/patches/nfc/customize.sh
+++ unica/patches/nfc/customize.sh
@@ -21,13 +21,13 @@ if [ "$TARGET_NFC_CHIP_VENDOR" = "SLSI" ]; then
     ln -sf "/system/lib64/libnfc_sec_jni.so" "$WORK_DIR/system/system/priv-app/NfcNci/lib/arm64/libnfc_sec_jni.so"
     SET_METADATA "system" "system/priv-app/NfcNci/lib/arm64/libnfc_sec_jni.so" 0 0 644 "u:object_r:system_file:s0"
 
-else
-
-if [ "$TARGET_NFC_CHIP_VENDOR" = "none" ]; then
+elif [ "$TARGET_NFC_CHIP_VENDOR" = "none" ]; then
     echo "Deleting NFC blobs"
 
     DELETE_FROM_WORK_DIR "system" "system/lib64/libnfc_nxpsn_jni.so"
     DELETE_FROM_WORK_DIR "system" "system/priv-app/NfcNci"
+    DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.nfc@2.0.so"
+    DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.nfc_aidl-V1-ndk.so"
 
 else  
     echo "NXP NFC found. Ignoring."
