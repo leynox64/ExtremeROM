ad04ac517584e6c52a24f5ca82d1424d6cbacae5
leynox64 <chinoidlacaille0@gmail.com>
2025-06-15 16:19:29 +0200

    platform: mt6789v: miscs: disable encryption

--- platform/mt6789v/patches/miscs/customize.sh
+++ platform/mt6789v/patches/miscs/customize.sh
@@ -5,5 +5,7 @@ SET_PROP "vendor" "debug.renderengine.backend" "skiagl"
 SET_PROP "vendor" "renderthread.skia.reduceopstasksplitting" "true"
 SET_PROP "vendor" "debug.hwui.skia_atrace_enabled" "false"
 
-echo "Disabling A/B"
-SET_PROP "product" "ro.product.ab_ota_partitions" --delete
+echo "Disabling encryption"
+# Encryption
+LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos2100")
+sed -i "${LINE}s/,fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized//g" "$WORK_DIR/vendor/etc/fstab.exynos2100"
