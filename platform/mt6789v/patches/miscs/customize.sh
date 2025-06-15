5afda6b47aadb36c1f66322e875b9c7441512690
leynox64 <chinoidlacaille0@gmail.com>
2025-06-15 16:22:00 +0200

    platform: mt6789v: miscs: fixup

--- platform/mt6789v/patches/miscs/customize.sh
+++ platform/mt6789v/patches/miscs/customize.sh
@@ -7,5 +7,5 @@ SET_PROP "vendor" "debug.hwui.skia_atrace_enabled" "false"
 
 echo "Disabling encryption"
 # Encryption
-LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos2100")
-sed -i "${LINE}s/,fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized//g" "$WORK_DIR/vendor/etc/fstab.exynos2100"
+LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.mt6789")
+sed -i "${LINE}s/,fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized//g" "$WORK_DIR/vendor/etc/fstab.mt6789"
