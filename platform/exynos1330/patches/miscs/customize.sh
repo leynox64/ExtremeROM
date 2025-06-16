88ddfe4535034eaa254ab14093a42206daeaba56
leynox64 <chinoidlacaille0@gmail.com>
2025-06-16 18:52:26 +0200

    platform: exynos1330: miscs: disable encryption

--- platform/exynos1330/patches/miscs/customize.sh
+++ platform/exynos1330/patches/miscs/customize.sh
@@ -5,11 +5,13 @@ SET_PROP "vendor" "debug.renderengine.backend" "skiagl"
 SET_PROP "vendor" "renderthread.skia.reduceopstasksplitting" "true"
 SET_PROP "vendor" "debug.hwui.skia_atrace_enabled" "false"
 
-echo "Disabling A/B"
-SET_PROP "product" "ro.product.ab_ota_partitions" --delete
-
 echo "Fix up /system/build.prop"
 SET_PROP "system" "ro.netflix.bsp_rev" --delete
 sed -i \
     "/ro.netflix.bsp_rev=EXYNOS1330-36497-1/i persist.audio.deepbuffer_delay=33" \
     "$WORK_DIR/system/system/build.prop"
+
+echo "Disabling encryption"
+# Encryption
+LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.s5e8535")
+sed -i "${LINE}s/,fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized//g" "$WORK_DIR/vendor/etc/fstab.s5e8535"
